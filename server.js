19b8d2c5091b292be58ca0ca1c89ed45cc076836
Kat Tschan <kat@wobbuffet.xyz>
2022-03-26 13:03:40 -0400

    Working changes that are actaully quite bad

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ var fs = require('fs');
 var url = require('url');
 var path = require('path');
 
-//Show the user voting.html on port 80, regardless of what page they request
+//Show the user the file they want on port 80
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    var filename = "./public/" + "voting.html";
+    var filename = "." + q.pathname;
     fs.readFile(filename, function(err, data) {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
         }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
-}).listen(80);
+    }).listen(80);
+
+
+
